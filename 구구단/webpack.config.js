245a93dfb43e1e0976fd4e14d11cbe6317ed673e
jawon0407 <jawon0407@naver.com>
2022-12-09 01:25:46 +0900

    복습 완

--- 구구단/webpack.config.js
+++ 구구단/webpack.config.js
@@ -2,26 +2,27 @@ const path = require('path');
 
 module.exports = {
     name : 'gugudan-setting',
-    mode : 'development', // 실서비스 : production  
-    devtool : 'eval',// 실서비스 : hidden-source-map
+    mode : 'development', // 실서비스 : production
+    devtool : 'eval', // 실전 : hidden-source-map
     resolve : {
-        extensions: ['.jx', '.jsx']
+        extensions : ['.js', '.jsx']
     },
-    entry:{
-        app: ['./client.jsx']
+    entry : {
+        app : './client',
     },
     module : {
-        rules:[{
+        rules : [{
             test : /\.jsx?/,
             loader : 'babel-loader',
             options : {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties']
+                presets : [
+                    '@babel/preset-env', '@babel/preset-react'
+                ]
             }
-        }],
+        }]
     },
-    output:{
+    output : {
         path : path.join(__dirname, 'dist'),
         filename : 'app.js'
-    }
+    }    
 }
--- 끝말잇기/webpack.config.js
+++ 끝말잇기/webpack.config.js
@@ -4,11 +4,23 @@ module.exports = {
     name : 'wordrelay-setting',
     mode : 'development', // 실서비스 : production
     devtool : 'eval',
-    
+    resolve : {
+        extensions : ['.js', '.jsx']
+    },    
     entry :{
-
+        app : './client',
     }, // 입력
+    module : {
+        rules : [{
+            test : /\.jsx?/,
+            loader : 'babel-loader',
+            options : {
+                presets : ['@babel/preset-env', '@babel/preset-react']
+            }
+        }]
+    }, // 모듈 처리 방식
     output:{
         path : path.join(__dirname, 'dist'),
+        filename : 'app.js'
     }, // 출력
 };
