0846a71e17e2d8f2415970f95f34fb7d50fe5147
jawon0407 <jawon0407@naver.com>
2022-12-14 22:50:19 +0900

    리렌더링 방지 작업

--- 반응속도체크/rendertest.jsx
+++ 반응속도체크/rendertest.jsx
@@ -1,23 +1,42 @@
-import React, {Component} from 'react';
+import React, { PureComponent } from "react";
+/* 
+    PureCompoenet는 state, props가 변경될 때 인지하고 렌더링을 새로해준다
+    하지만 PureComponent 는 class 기반 컴포넌트에서만 사용할 수 있다.
+*/
+class RenderTest extends PureComponent {
+  state = {
+    counter: 0,
+  };
 
-class RenderTest extends Component {
-    state={
-        counter : 0,
-    }
+  /* 
+    shouldComponentUpdate()
+    렌더링이 되는 경우를 설정해줘야한다.
+    => 최적화 관련 
+    true 일 경우 render false일 경우 render X 
+  
+    shouldComponentUpdate(nextProps, nextState, nextContext) {
+        if (this.state.counter !== nextState.counter) {
+            return true;
+            }
+            return false;
+        }
+*/
 
-    counterIncrease = (e) => {
-        e.preventDefault();
-        this.setState({})
+  counterIncrease = (e) => {
+    e.preventDefault();
+    this.setState({});
+  };
+
+  render() {
+    {
+      console.log("렌더링", this.state.counter);
     }
-    
-    render(){
-        {console.log('렌더링', this.state.counter)}
-        return(
-           <>
-              <button onClick={this.counterIncrease}>클릭</button>
-           </> 
-        )
-    }
+    return (
+      <>
+        <button onClick={this.counterIncrease}>클릭</button>
+      </>
+    );
+  }
 }
 
 export default RenderTest;
